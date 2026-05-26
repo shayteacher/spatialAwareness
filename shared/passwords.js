ddda775ed32d931e89fd9de0fe5fb53347534c4f
shayteacher <shay.teacher.contact@gmail.com>
2026-05-26 21:14:15 +0900

    Update passwords.js
    Added New Password for Level 35

--- shared/passwords.js
+++ shared/passwords.js
@@ -1,7 +1,8 @@
 const SECTION_PASSWORDS = {
-  level1: "CANDY01",
-  level2: "SCENE02",
-  level3: "CHALK03",
-  level4: "TAPE04",
-  level5: "SOLVED5"
+  level1:  "CANDY01",
+  level2:  "SCENE02",
+  level3:  "CHALK03",
+  level35: "BRIEF035",
+  level4:  "TAPE04",
+  level5:  "SOLVED5"
 };
